b3eb0564593c42dcb7aa83da6a8537cf19c8de76
yeefea <yeefea@126.com>
2023-02-11 02:03:48 +0800

    update: acpp

--- src/acpp/ch2.cpp
+++ src/acpp/ch2.cpp
@@ -1,61 +1,87 @@
 #include <iostream>
 
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
 void demo_while_loop()
 {
-    std::cout << "enter your first name: ";
-    std::string name;
-    std::cin >> name;
-    const std::string greeting = "hello, " + name + "!";
+    cout << "enter your first name: ";
+    string name;
+    cin >> name;
+    const string greeting = "hello, " + name + "!";
 
     const int pad = 1;
     const int rows = pad * 2 + 3;
+    const string::size_type cols = greeting.size() + pad * 2 + 2;
 
-    std::cout << std::endl;
+    cout << endl;
 
-    const std::string::size_type cols = greeting.size() + pad * 2 + 2;
-    int r = 0;
-    while (r != rows)
+    for (int r = 0; r != rows; ++r)
     {
+        string::size_type c = 0;
 
-        std::cout << std::endl;
-        ++r;
-    }
-
-    std::string::size_type c = 0;
-    while (c != cols)
-    {
-        if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1)
+        while (c != cols)
         {
-            std::cout << "*";
-        }
-        else
-        {
+            // is it time to write the greeting?
             if (r == pad + 1 && c == pad + 1)
             {
-                std::cout << greeting;
+                cout << greeting;
                 c += greeting.size();
             }
             else
             {
-                std::cout << " ";
+                // are we on the border?
+                if (r == 0 || r == rows - 1 ||
+                    c == 0 || c == cols - 1)
+                    cout << "*";
+                else
+                    cout << " ";
+                ++c;
             }
         }
+        cout << endl;
     }
-}
-
-void demo_if()
-{
 }
 
 void demo_for_loop()
 {
+    const int rows = 10;
+    for (int r = 1; r <= rows; ++r)
+    {
+        cout << r << " ";
+    }
 
-    
+    cout << endl;
 }
 
+void demo_types()
+{
+
+    bool b = true;
+
+    unsigned u = 10;
+    short s = 11;
+    long l = 12;
+    size_t z = 100;
+    string::size_type sz = 101;
+    cout << "bool " << b << endl;
+    cout << "unsigned " << u << endl;
+    cout << "short " << s << endl;
+    cout << "long " << l << endl;
+    cout << "size_t " << z << endl;
+    cout << "string::size_type " << sz << endl;
+
+    // strings
+    string s0;
+    string s1 = "a";
+    string s2(100, 'c');
+}
 int main()
 {
-    demo_while_loop();
-    demo_if();
+    // demo_while_loop();
+    // demo_for_loop();
+    demo_types();
     return 0;
 }
--- src/acpp/ch3.cpp
+++ src/acpp/ch3.cpp
@@ -3,3 +3,94 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::setprecision;
+using std::streamsize;
+using std::string;
+using std::vector;
+
+void demo_grades()
+{
+    // ask for and read the student's name
+    cout << "Please enter your first name: ";
+    string name;
+    cin >> name;
+    cout << "Hello, " << name << "!" << endl;
+    // ask for and read the midterm and final grades
+    cout << "Please enter your midterm and final exam grades: ";
+    double midterm, final;
+    cin >> midterm >> final;
+    // ask for the homework grades
+    cout << "Enter all your homework grades, "
+            "followed by end-of-file: ";
+    // the number and sum of grades read so far
+    int count = 0;
+    double sum = 0;
+    // a variable into which to read
+    double x;
+
+    // invariant:
+    // we have read count grades so far, and
+    // sum is the sum of the first count grades
+    while (cin >> x)
+    {
+        ++count;
+        sum += x;
+    }
+    // write the result
+    streamsize prec = cout.precision();
+
+    cout << "Your final grade is " << setprecision(3)
+         << 0.2 * midterm + 0.4 * final + 0.4 * sum / count
+         << setprecision(prec) << endl;
+}
+
+
+
+void demo_grades2()
+{
+    // ask for and read the student's name
+    cout << "Please enter your first name: ";
+    string name;
+    cin >> name;
+    cout << "Hello, " << name << "!" << endl;
+    // ask for and read the midterm and final grades
+    cout << "Please enter your midterm and final exam grades: ";
+    double midterm, final;
+    cin >> midterm >> final;
+    // ask for the homework grades
+    cout << "Enter all your homework grades, "
+            "followed by end-of-file: ";
+    // the number and sum of grades read so far
+    int count = 0;
+    double sum = 0;
+    // a variable into which to read
+    double x;
+
+    vector<double> homework;
+
+    // invariant:
+    // we have read count grades so far, and
+    // sum is the sum of the first count grades
+    while (cin >> x)
+    {
+        homework.push_back(x);
+    }
+    sort(homework.begin(), homework.end());
+    // write the result
+    streamsize prec = cout.precision();
+
+    cout << "Your final grade is " << setprecision(3)
+         << 0.2 * midterm + 0.4 * final + 0.4 * sum / count
+         << setprecision(prec) << endl;
+}
+
+int main()
+{
+    // demo_grades();
+    demo_grades2();
+    return 0;
+}
